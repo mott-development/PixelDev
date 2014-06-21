dc9ed9cb6e38b19ef48c01c33b20c73bd782b83e
tajpw <tajpw@hotmail.com>
2014-06-21 14:29:47 +1000

    Crap

--- Java Code/src/com/mott/pixelsquad/main/Game.java
+++ Java Code/src/com/mott/pixelsquad/main/Game.java
@@ -4,18 +4,26 @@ import javax.swing.JFrame;
 
 public class Game {
 
-	private static final int INIT_WIDTH = 1280;
+	private static final String NAME = "Team Pixel";
+
+	private static final int INIT_WIDTH = 800;
 	private static final int INIT_HEIGHT = INIT_WIDTH / 16 * 9;
 
 	public static void main(String[] args) {
+
+		new Game();
+
+	}
+
+	public Game() {
 
 		// Initializing
 		JFrame frame = new JFrame();
 		frame.setSize(INIT_WIDTH, INIT_HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setTitle(NAME);
 		frame.setVisible(true);
-		frame.setTitle("");
 
 	}
-
 }
